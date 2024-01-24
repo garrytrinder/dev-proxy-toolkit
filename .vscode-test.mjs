74f819b0c8261dd9eca90450ff3ac5825f92470f
Garry Trinder <garry@trinder365.co.uk>
2024-01-24 22:19:43 +0000

    Use Insiders edition in test runner

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -2,6 +2,7 @@ import {defineConfig} from '@vscode/test-cli';
 
 export default defineConfig({
   files: 'out/test/**/*.test.js',
+  version: 'insiders',
   mocha: {
     ui: 'tdd',
     timeout: 20000,
