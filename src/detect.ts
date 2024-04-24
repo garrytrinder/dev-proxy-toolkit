b81c1e6804cb73145b742e9d6675f22b474ca1c1
Garry Trinder <garry@trinder365.co.uk>
2024-04-24 14:45:35 +0100

    Add version preference setting. Closes #71
    Closes #71

--- src/detect.ts
+++ src/detect.ts
@@ -1,10 +1,12 @@
+import * as vscode from 'vscode';
 import { exec } from 'child_process';
 import { DevProxyInstall } from './types';
 import os from 'os';
+import { VersionExeName, VersionPreference } from './enums';
 
-export const getVersion = async () => {
+export const getVersion = async (devProxyExe: string) => {
     try {
-        const version = await executeCommand(`devproxy --version`);
+        const version = await executeCommand(`${devProxyExe} --version`);
         return version.trim();
     } catch (error) {
         return "";
@@ -25,14 +27,15 @@ export const executeCommand = async (cmd: string): Promise<string> => {
     });
 };
 
-export const detectDevProxyInstall = async (): Promise<DevProxyInstall> => {
-    const version = await getVersion();
+export const detectDevProxyInstall = async (versionPreference: VersionPreference): Promise<DevProxyInstall> => {
+    const devProxyExe = getDevProxyExe(versionPreference);
+    const version = await getVersion(devProxyExe);
     const isInstalled = version !== '';
     const isBeta = version.includes('beta');
     const platform = os.platform();
-    const outdatedVersion = await getOutdatedVersion();
+    const outdatedVersion = await getOutdatedVersion(devProxyExe);
     const isOutdated = isInstalled && outdatedVersion !== '';
-    const isRunning = await isDevProxyRunning();
+    const isRunning = await isDevProxyRunning(devProxyExe);
 
     return {
         version,
@@ -45,25 +48,31 @@ export const detectDevProxyInstall = async (): Promise<DevProxyInstall> => {
     };
 };
 
-export const getOutdatedVersion = async (): Promise<string> => {
+export const getOutdatedVersion = async (devProxyExe: string): Promise<string> => {
     try {
-        const outdated = await executeCommand(`devproxy outdated --short`);
+        const outdated = await executeCommand(`${devProxyExe} outdated --short`);
         return outdated ? outdated.trim() : '';
     } catch (error) {
         return "";
     }
 };
 
-export const isDevProxyRunning = async (): Promise<boolean> => {
+export const isDevProxyRunning = async (devProxyExe: string): Promise<boolean> => {
     const platform = os.platform();
 
     if (platform === 'win32') {
-        const processId = await executeCommand('pwsh.exe -c "(Get-Process devproxy -ErrorAction SilentlyContinue).Id"');
+        const processId = await executeCommand(`pwsh.exe -c "(Get-Process ${devProxyExe} -ErrorAction SilentlyContinue).Id"`);
         return processId.trim() !== '';
     };
     if (platform === 'darwin') {
-        const processId = await executeCommand('$SHELL -c "ps -ef | grep devproxy | grep -v grep | awk \'{print $2}\'"');
+        const processId = await executeCommand(`$SHELL -c "ps -ef | grep ${devProxyExe} | grep -v grep | awk \'{print $2}\'"`);
         return processId.trim() !== '';
     };
     return false;
+};
+
+export const getDevProxyExe = (versionPreference: VersionPreference) => {
+    return versionPreference === VersionPreference.Stable
+        ? VersionExeName.Stable
+        : VersionExeName.Beta;
 };
--- src/extension.ts
+++ src/extension.ts
@@ -6,10 +6,14 @@ import { registerCodeLens } from './codelens';
 import { createStatusBar, statusBarLoop, updateStatusBar } from './statusbar';
 import { registerCodeActions } from './codeactions';
 import { updateGlobalState } from './state';
+import { VersionPreference } from './enums';
 
 export const activate = async (context: vscode.ExtensionContext): Promise<vscode.ExtensionContext> => {
+  const configuration = vscode.workspace.getConfiguration('devproxytoolkit');
+  const versionPreference = configuration.get('versionPreference') as VersionPreference;
+
   const statusBar = createStatusBar(context);
-  await updateGlobalState(context);
+  await updateGlobalState(context, versionPreference);
 
   const collection = vscode.languages.createDiagnosticCollection('Dev Proxy');
 
@@ -20,10 +24,10 @@ export const activate = async (context: vscode.ExtensionContext): Promise<vscode
 
   const notification = handleStartNotification(context);
   processNotification(notification);
-  
+
   updateStatusBar(context, statusBar);
 
-  setInterval(() => statusBarLoop(context, statusBar), 5000);
+  setInterval(() => statusBarLoop(context, statusBar, versionPreference), 5000);
 
   return context;
 };
