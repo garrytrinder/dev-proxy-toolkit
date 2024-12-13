f456e194b28d87795f34a0900e7c6df6474ecb7c
Garry Trinder <garry@trinder365.co.uk>
2024-12-13 16:21:10 +0000

    Refactor diagnostic update functions for better clarity and consistency

--- src/diagnostics.ts
+++ src/diagnostics.ts
@@ -4,7 +4,7 @@ import { pluginSnippets } from "./constants";
 import { getASTNode, getRangeFromASTNode } from "./helpers";
 import { DevProxyInstall } from './types';
 
-export const updateConfigDiagnostics = (
+export const updateConfigFileDiagnostics = (
   context: vscode.ExtensionContext,
   document: vscode.TextDocument,
   collection: vscode.DiagnosticCollection,
@@ -14,145 +14,16 @@ export const updateConfigDiagnostics = (
     return;
   }
   const diagnostics: vscode.Diagnostic[] = [];
-  const documentNode = parse(document.getText()) as parse.ObjectNode;
+  const documentNode = getObjectNodeFromDocument(document);
+  const pluginsNode = getPluginsNode(documentNode);
 
-  // check if schema version is compatible
   checkSchemaCompatibility(documentNode, devProxyInstall, diagnostics);
-
-  // check validity of plugins
-  const pluginsNode = getASTNode(
-    documentNode.children,
-    'Identifier',
-    'plugins'
-  );
-  if (
-    pluginsNode &&
-    (pluginsNode.value as parse.ArrayNode).children.length !== 0
-  ) {
-    const pluginNodes = (pluginsNode.value as parse.ArrayNode)
-      .children as parse.ObjectNode[];
-
-    // check for plugins
-    if (pluginNodes.length === 0) {
-      diagnostics.push(
-        new vscode.Diagnostic(
-          getRangeFromASTNode(pluginsNode),
-          'Add at least one plugin',
-          vscode.DiagnosticSeverity.Error
-        )
-      );
-    }
-
-    // check if we have any plugins that contain the name reporter in the plugins node
-    const reporterIndex = pluginNodes.findIndex((pluginNode: parse.ObjectNode) => {
-      const pluginNameNode = getASTNode(
-        pluginNode.children,
-        'Identifier',
-        'name'
-      );
-      const pluginName = (pluginNameNode?.value as parse.LiteralNode)
-        .value as string;
-      return pluginName.toLowerCase().includes('reporter');
-    });
-
-    if (reporterIndex !== -1) {
-      // check if we have any more plugins after the reporter plugin
-      const pluginsAfterReporter = pluginNodes.slice(reporterIndex + 1);
-      // if we do, add a warning to the reporter plugin stating that it should be the last plugin
-      if (pluginsAfterReporter.length > 0) {
-        // check if there are any plugins after the reporter plugin that are not reporters
-        const pluginAfterReporter = pluginsAfterReporter.find((pluginNode: parse.ObjectNode) => {
-          const pluginNameNode = getASTNode(
-            pluginNode.children,
-            'Identifier',
-            'name'
-          );
-          const pluginName = (pluginNameNode?.value as parse.LiteralNode)
-            .value as string;
-          return !pluginName.toLowerCase().includes('reporter');
-        });
-        // if there are, add a warning to the reporter plugin
-        if (pluginAfterReporter) {
-          const diagnostic = new vscode.Diagnostic(
-            getRangeFromASTNode(pluginNodes[reporterIndex]),
-            'Reporters should be placed after other plugins.',
-            vscode.DiagnosticSeverity.Warning
-          );
-          diagnostics.push(diagnostic);
-        }
-      }
-    }
-
-    // does the plugin have a config section?
-    pluginNodes.forEach((pluginNode: parse.ObjectNode) => {
-      const pluginNameNode = getASTNode(
-        pluginNode.children,
-        'Identifier',
-        'name'
-      );
-      const pluginName = (pluginNameNode?.value as parse.LiteralNode)
-        .value as string;
-      const enabledNode = getASTNode(
-        pluginNode.children,
-        'Identifier',
-        'enabled'
-      );
-      const isEnabled = (enabledNode?.value as parse.LiteralNode)
-        .value as boolean;
-      const pluginSnippet = pluginSnippets[pluginName];
-      const requiresConfig = pluginSnippet.config
-        ? pluginSnippet.config.required
-        : false;
-
-      if (requiresConfig) {
-        // check to see if the plugin has a config section
-        const configSectionNode = getASTNode(
-          pluginNode.children,
-          'Identifier',
-          'configSection'
-        );
-        if (!configSectionNode) {
-          // there is no config section defined on the plugin instance
-          diagnostics.push(
-            new vscode.Diagnostic(
-              getRangeFromASTNode(pluginNode),
-              `${pluginName} requires a config section.`,
-              isEnabled
-                ? vscode.DiagnosticSeverity.Error
-                : vscode.DiagnosticSeverity.Warning
-            )
-          );
-        } else {
-          // check to see if the config section is in the document
-          const configSectionName = (
-            configSectionNode.value as parse.LiteralNode
-          ).value as string;
-          const configSection = getASTNode(
-            documentNode.children,
-            'Identifier',
-            configSectionName
-          );
-
-          if (!configSection) {
-            diagnostics.push(
-              new vscode.Diagnostic(
-                getRangeFromASTNode(configSectionNode.value),
-                `${configSectionName} config section is missing. Use '${pluginSnippet.config?.name}' snippet to create one.`,
-                isEnabled
-                  ? vscode.DiagnosticSeverity.Error
-                  : vscode.DiagnosticSeverity.Warning
-              )
-            );
-          }
-        }
-      }
-    });
-  }
+  checkPlugins(pluginsNode, diagnostics, documentNode);
 
   collection.set(document.uri, diagnostics);
 };
 
-export const updateDiagnostics = (
+export const updateFileDiagnostics = (
   context: vscode.ExtensionContext,
   document: vscode.TextDocument,
   collection: vscode.DiagnosticCollection,
@@ -163,15 +34,14 @@ export const updateDiagnostics = (
   }
 
   const diagnostics: vscode.Diagnostic[] = [];
-  const documentNode = parse(document.getText()) as parse.ObjectNode;
+  const documentNode = getObjectNodeFromDocument(document);
 
-  // check if schema version is compatible
   checkSchemaCompatibility(documentNode, devProxyInstall, diagnostics);
 
   collection.set(document.uri, diagnostics);
 };
 
-export const checkSchemaCompatibility = (documentNode: parse.ObjectNode, devProxyInstall: DevProxyInstall, diagnostics: vscode.Diagnostic[]) => {
+const checkSchemaCompatibility = (documentNode: parse.ObjectNode, devProxyInstall: DevProxyInstall, diagnostics: vscode.Diagnostic[]) => {
   const schemaNode = getASTNode(documentNode.children, 'Identifier', '$schema');
   if (schemaNode) {
     const schemaValue = (schemaNode.value as parse.LiteralNode).value as string;
@@ -187,3 +57,182 @@ export const checkSchemaCompatibility = (documentNode: parse.ObjectNode, devProx
     }
   }
 };
+
+const checkPlugins = (pluginsNode: parse.PropertyNode | undefined, diagnostics: vscode.Diagnostic[], documentNode: parse.ObjectNode) => {
+  if (pluginsNode &&
+    (pluginsNode.value as parse.ArrayNode)) {
+    const pluginNodes = (pluginsNode.value as parse.ArrayNode)
+      .children as parse.ObjectNode[];
+
+    checkAtLeastOneEnabledPlugin(pluginNodes, diagnostics, pluginsNode);
+    warnOnReporterPosition(pluginNodes, diagnostics);
+    validatePluginConfigurations(pluginNodes, diagnostics, documentNode);
+  }
+};
+
+const validatePluginConfigurations = (pluginNodes: parse.ObjectNode[], diagnostics: vscode.Diagnostic[], documentNode: parse.ObjectNode) => {
+  pluginNodes.forEach((pluginNode: parse.ObjectNode) => {
+    const pluginNameNode = getASTNode(
+      pluginNode.children,
+      'Identifier',
+      'name'
+    );
+    const pluginName = (pluginNameNode?.value as parse.LiteralNode)
+      .value as string;
+    const enabledNode = getASTNode(
+      pluginNode.children,
+      'Identifier',
+      'enabled'
+    );
+    const isEnabled = (enabledNode?.value as parse.LiteralNode)
+      .value as boolean;
+    const pluginSnippet = pluginSnippets[pluginName];
+
+    checkPluginConfiguration(pluginNode, diagnostics, pluginName, isEnabled, documentNode, pluginSnippet);
+  });
+};
+
+const warnOnReporterPosition = (pluginNodes: parse.ObjectNode[], diagnostics: vscode.Diagnostic[]) => {
+  const reporterIndex = pluginNodes.findIndex((pluginNode: parse.ObjectNode) => {
+    const pluginNameNode = getASTNode(
+      pluginNode.children,
+      'Identifier',
+      'name'
+    );
+    const pluginName = (pluginNameNode?.value as parse.LiteralNode)
+      .value as string;
+    return pluginName.toLowerCase().includes('reporter');
+  });
+
+  if (reporterIndex !== -1) {
+    // check if we have any more plugins after the reporter plugin
+    const pluginsAfterReporter = pluginNodes.slice(reporterIndex + 1);
+    // if we do, add a warning to the reporter plugin stating that it should be the last plugin
+    if (pluginsAfterReporter.length > 0) {
+      // check if there are any plugins after the reporter plugin that are not reporters
+      const pluginAfterReporter = pluginsAfterReporter.find((pluginNode: parse.ObjectNode) => {
+        const pluginNameNode = getASTNode(
+          pluginNode.children,
+          'Identifier',
+          'name'
+        );
+        const pluginName = (pluginNameNode?.value as parse.LiteralNode)
+          .value as string;
+        return !pluginName.toLowerCase().includes('reporter');
+      });
+      // if there are, add a warning to the reporter plugin
+      if (pluginAfterReporter) {
+        const diagnostic = new vscode.Diagnostic(
+          getRangeFromASTNode(pluginNodes[reporterIndex]),
+          'Reporters should be placed after other plugins.',
+          vscode.DiagnosticSeverity.Warning
+        );
+        diagnostics.push(diagnostic);
+      }
+    }
+  }
+};
+
+const checkAtLeastOneEnabledPlugin = (pluginNodes: parse.ObjectNode[], diagnostics: vscode.Diagnostic[], pluginsNode: parse.PropertyNode) => {
+  // check if there are any plugins
+  if (pluginNodes.length === 0) {
+    diagnostics.push(
+      new vscode.Diagnostic(
+        getRangeFromASTNode(pluginsNode),
+        'Add at least one plugin',
+        vscode.DiagnosticSeverity.Error
+      )
+    );
+  } else {
+    // check if there are any enabled plugins
+    const enabledPlugins = pluginNodes.filter((pluginNode: parse.ObjectNode) => {
+      const enabledNode = getASTNode(
+        pluginNode.children,
+        'Identifier',
+        'enabled'
+      );
+      return (enabledNode?.value as parse.LiteralNode).value as boolean;
+    });
+    if (enabledPlugins.length === 0) {
+      diagnostics.push(
+        new vscode.Diagnostic(
+          getRangeFromASTNode(pluginsNode),
+          'At least one plugin must be enabled',
+          vscode.DiagnosticSeverity.Error
+        )
+      );
+    }
+  }
+};
+
+const checkPluginConfiguration = (pluginNode: parse.ObjectNode, diagnostics: vscode.Diagnostic[], pluginName: string, isEnabled: boolean, documentNode: parse.ObjectNode, pluginSnippet: { instance: string; config?: import("/Users/garrytrinder/repos/dev-proxy-toolkit/src/types").PluginConfig; }) => {
+  const configSectionNode = getASTNode(
+    pluginNode.children,
+    'Identifier',
+    'configSection'
+  );
+
+  // if the plugin does not require a config section, we should not have one
+  if (!pluginSnippet.config && configSectionNode) {
+    diagnostics.push(
+      new vscode.Diagnostic(
+        getRangeFromASTNode(configSectionNode),
+        `${pluginName} does not require a config section.`,
+        isEnabled
+          ? vscode.DiagnosticSeverity.Error
+          : vscode.DiagnosticSeverity.Warning
+      )
+    );
+    return;
+  }
+
+  // if there is no config section defined on the plugin, we should have one if the plugin requires it
+  if (!configSectionNode) {
+    if (pluginSnippet.config?.required) {
+      diagnostics.push(
+        new vscode.Diagnostic(
+          getRangeFromASTNode(pluginNode),
+          `${pluginName} requires a config section.`,
+          isEnabled
+            ? vscode.DiagnosticSeverity.Error
+            : vscode.DiagnosticSeverity.Warning
+        )
+      );
+    }
+  } else {
+    // if there is a config section defined on the plugin, we should have the config section defined in the document
+    const configSectionName = (
+      configSectionNode.value as parse.LiteralNode
+    ).value as string;
+    const configSection = getASTNode(
+      documentNode.children,
+      'Identifier',
+      configSectionName
+    );
+
+    if (!configSection) {
+      diagnostics.push(
+        new vscode.Diagnostic(
+          getRangeFromASTNode(configSectionNode.value),
+          `${configSectionName} config section is missing. Use '${pluginSnippet.config?.name}' snippet to create one.`,
+          isEnabled
+            ? vscode.DiagnosticSeverity.Error
+            : vscode.DiagnosticSeverity.Warning
+        )
+      );
+    }
+  }
+};
+
+const getPluginsNode = (documentNode: parse.ObjectNode) => {
+  return getASTNode(
+    documentNode.children,
+    'Identifier',
+    'plugins'
+  );
+};
+
+const getObjectNodeFromDocument = (document: vscode.TextDocument): parse.ObjectNode => {
+  return parse(document.getText()) as parse.ObjectNode;
+};
+
