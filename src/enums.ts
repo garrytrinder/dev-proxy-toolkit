d5100d8eef34987e9eb97fd82c4269981844b997
Garry Trinder <garry@trinder365.co.uk>
2025-07-08 16:17:33 +0100

    Update winget package identifiers and package identifier retrieval. Closes #300

--- src/enums.ts
+++ src/enums.ts
@@ -4,6 +4,21 @@ export enum VersionPreference {
 }
 
 export enum VersionExeName {
-    Stable = 'devproxy',
-    Beta = 'devproxy-beta',
+  Stable = 'devproxy',
+  Beta = 'devproxy-beta',
+}
+
+export enum WingetPackageIdentifier {
+  Stable = 'DevProxy.DevProxy',
+  Beta = 'DevProxy.DevProxy.Beta',
+}
+
+export enum HomebrewPackageIdentifier {
+  Stable = 'devproxy',
+  Beta = 'devproxy-beta',
+}
+
+export enum PackageManager {
+  Winget,
+  Homebrew,
 }
